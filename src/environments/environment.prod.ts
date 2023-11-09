eb2d7251869f85bef1a301609e4428637856c599
SandroVolski <sandroeduardopradovolski@gmail.com>
2023-11-09 08:58:19 -0300

    Inicio

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDU-CfW09OIPuMyp6JqD29pcvo7rcp2Eeg",
+    authDomain: "cm-contatos-58330.firebaseapp.com",
+    projectId: "cm-contatos-58330",
+    storageBucket: "cm-contatos-58330.appspot.com",
+    messagingSenderId: "696313706498",
+    appId: "1:696313706498:web:ed936ceb98cebe43fbcf1b"
+  }
+};
